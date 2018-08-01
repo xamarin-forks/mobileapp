27c7beafd240548820db4362643d11d94014c1c4
Ricardo Sanchez <odrakir@gmail.com>
2018-08-01 10:40:31 +0200

    Add Reactive class

---
+++ Toggl.Daneel/Extensions/UIKitRxExtensions/Reactive.cs
@@ -0,0 +1,20 @@
+﻿namespace Toggl.Daneel.Extensions
+{
+    public class Reactive<TBase>
+    {
+        public TBase Base { get; set; }
+
+        public Reactive(TBase _base)
+        {
+            Base = _base;
+        }
+    }
+
+    public static class NSObjectReactiveCompatibleExtensions
+    {
+        public static Reactive<NSObject> Rx<NSObject>(this NSObject nsObject)
+            => new Reactive<NSObject>(nsObject);
+    }
+}
+
+
