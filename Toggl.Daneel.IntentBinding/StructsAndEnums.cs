00dda437039f0db6cce8d237834cdbdb4d892442
khoi <khoiracle@gmail.com>
2018-08-24 10:46:03 +0700

    ⚔️ Add IntentBinding project

---
+++ Toggl.Daneel.IntentBinding/StructsAndEnums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace Toggl.Daneel.Intents
+{
+//    [Watch (5,0), iOS (12,0)]
+    [Native]
+    public enum StopTimerIntentResponseCode : long
+    {
+        Unspecified = 0,
+        Ready,
+        ContinueInApp,
+        InProgress,
+        Success,
+        Failure,
+        FailureRequiringAppLaunch
+    }
+}
