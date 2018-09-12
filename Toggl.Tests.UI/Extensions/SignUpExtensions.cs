55e1e74ceea10c0169fa191d847f1e95adf18969
William Barbosa <william.sb@hotmail.com>
2018-09-12 17:47:46 -0300

    :tada: Update onboarding tests

---
+++ Toggl.Tests.UI/Extensions/SignUpExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.UITest;
+
+namespace Toggl.Tests.UI.Extensions
+{
+    public static class SignUpExtensions
+    {
+        public static void WaitForSignUpScreen(this IApp app)
+        {
+            app.WaitForLoginScreen();
+            app.Tap(Login.SwitchToSignUpLabel);
+            app.WaitForElement(SignUp.SignUpButton);
+        }
+
+        public static void TrySigningUpAndFail(this IApp app)
+        {
+            app.Tap(Login.LoginButton);
+
+            app.WaitForElement(Login.ErrorLabel);
+        }
+
+        public static void SignUpSuccesfully(this IApp app)
+        {
+            app.Tap(Login.LoginButton);
+
+            app.WaitForElement(Main.StartTimeEntryButton);
+        }
+    }
+}
