158e6804aa1536415e85bf86f96b625adbf4c5e0
Simon Rozsival <simon@rozsival.com>
2018-09-21 09:55:06 +0200

    :construction: Move tags migration to a separate class

---
+++ Toggl.PrimeRadiant.Realm/Migrations/TagServerDeletedAt.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+
+namespace Toggl.PrimeRadiant.Realm.Migrations
+{
+    public sealed class TagServerDeletedAt : IMigration
+    {
+        public ulong TargetSchemaVersion { get; } = 4;
+
+        public void PerformMigration(Realms.Realm oldRealm, Realms.Realm newRealm)
+        {
+            var newTags = newRealm.All<RealmTag>();
+            var oldTags = oldRealm.All("RealmTag");
+            for (var i = 0; i < newTags.Count(); i++)
+            {
+                var oldTag = oldTags.ElementAt(i);
+                var newTag = newTags.ElementAt(i);
+                newTag.ServerDeletedAt = oldTag.DeletedAt;
+            }
+        }
+    }
+}
