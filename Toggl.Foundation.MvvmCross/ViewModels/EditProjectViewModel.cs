7a24ca89146805b39d9a192430e8c14e08f85336
khoi <khoiracle@gmail.com>
2018-09-24 13:12:07 +0700

    🚧 default project has higher priority

--- Toggl.Foundation.MvvmCross/ViewModels/EditProjectViewModel.cs
+++ Toggl.Foundation.MvvmCross/ViewModels/EditProjectViewModel.cs
@@ -106,8 +106,11 @@ namespace Toggl.Foundation.MvvmCross.ViewModels
 
         public override async Task Initialize()
         {
+            var defaultWorkspace = await interactorFactory.GetDefaultWorkspace().Execute();
             var allWorkspaces = await interactorFactory.GetAllWorkspaces().Execute();
-            var workspace = allWorkspaces.First(ws => ws.IsEligibleForProjectCreation());
+            var workspace = defaultWorkspace.IsEligibleForProjectCreation()
+                ? defaultWorkspace
+                : allWorkspaces.First(ws => ws.IsEligibleForProjectCreation());
             areCustomColorsEnabled = await interactorFactory.AreCustomColorsEnabledForWorkspace(workspace.Id).Execute();
             workspaceId = initialWorkspaceId = workspace.Id;
             WorkspaceName = workspace.Name;
