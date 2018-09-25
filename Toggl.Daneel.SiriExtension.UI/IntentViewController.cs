b7b20f9d93bf19458e7d388c5cd3d2b623c30a20
Ricardo Sanchez <odrakir@gmail.com>
2018-09-25 18:45:12 +0300

    Use autolayout and attributed strings

--- Toggl.Daneel.SiriExtension.UI/IntentViewController.cs
+++ Toggl.Daneel.SiriExtension.UI/IntentViewController.cs
@@ -12,6 +12,10 @@ namespace Toggl.Daneel.SiriExtension.UI
 {
     public partial class IntentViewController : UIViewController, IINUIHostedViewControlling
     {
+
+        private UIStringAttributes boldAttributes;
+        private UIStringAttributes regularAttributes;
+
         protected IntentViewController(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
@@ -25,64 +29,105 @@ namespace Toggl.Daneel.SiriExtension.UI
                  INUIHostedViewContext context,
                  INUIHostedViewControllingConfigureViewHandler completion)
         {
+            var paragraphStyle = new NSMutableParagraphStyle();
+            paragraphStyle.LineSpacing = 2;
+
+            boldAttributes = new UIStringAttributes
+            {
+                Font = UIFont.BoldSystemFontOfSize(15),
+                ForegroundColor = UIColor.Black,
+                ParagraphStyle = paragraphStyle
+            };
+
+            regularAttributes = new UIStringAttributes
+            {
+                Font = UIFont.SystemFontOfSize(15),
+                ForegroundColor = UIColor.Black,
+                ParagraphStyle = paragraphStyle
+            };
+
+            foreach (var view in View.Subviews)
+            {
+                view.RemoveFromSuperview();
+            }
+
+            var success = true;
+            var desiredSize = CGSize.Empty;
+
             switch (interaction.Intent)
             {
                 case StartTimerIntent startTimerIntent:
-                    var desiredSize = CGSize.Empty;
-
                     if (interaction.IntentHandlingStatus == INIntentHandlingStatus.Success)
                     {
-                        showStartTimerSuccess(startTimerIntent.EntryDescription);
-                        desiredSize = new CGSize(200, 60);
+                        desiredSize = showStartTimerSuccess(startTimerIntent.EntryDescription);                         
                     }
-
-                    completion(true, parameters, desiredSize);
                     break;
-                case StopTimerIntent _:
-                    desiredSize = CGSize.Empty;
 
+                case StopTimerIntent _:
                     if (interaction.IntentHandlingStatus == INIntentHandlingStatus.Success)
                     {
                         var response = interaction.IntentResponse as StopTimerIntentResponse;
                         if (!(response is null))
                         {
-                            showStopResponse(response);
-                            desiredSize = new CGSize(300, 60);
+                            desiredSize = showStopResponse(response);
                         }
                     }
-                    completion(true, parameters, desiredSize);
+
                     break;
                 default:
-                    completion(false, new NSSet<INParameter>(), CGSize.Empty);
+                    success = false;
                     break;
             }
+
+            completion(success, parameters, desiredSize);
         }
 
-        private void showStartTimerSuccess(string description)
+        private CGSize showStartTimerSuccess(string description)
         {
-            descriptionLabel.Text = string.IsNullOrEmpty(description) ? "No Description" : description;
-            timeLabel.Text = "";
-            timeFrameLabel.Text = "";
+            entryInfoView.DescriptionLabel.Text = "";
+            entryInfoView.TimeLabel.Text = "";
+
+            var attributedString = new NSMutableAttributedString(string.IsNullOrEmpty(description) ? "No Description" : description);
+            entryInfoView.DescriptionLabel.AttributedText = attributedString;
 
             var start = DateTimeOffset.Now;
             var displayLink = CADisplayLink.Create(() => {
                 var passed = DateTimeOffset.Now - start;
-                timeLabel.Text = secondsToString(passed.Seconds);
+                entryInfoView.TimeLabel.Text = secondsToString(passed.Seconds);
             });
             displayLink.AddToRunLoop(NSRunLoop.Current, NSRunLoopMode.Default);
+
+            View.AddSubview(entryInfoView);
+            var width = this.ExtensionContext?.GetHostedViewMaximumAllowedSize().Width ?? 320;
+            var frame = new CGRect(0, 0, width, 60);
+            entryInfoView.Frame = frame;
+
+            return frame.Size;
         }
 
-        private void showStopResponse(StopTimerIntentResponse response)
+        private CGSize showStopResponse(StopTimerIntentResponse response)
         {
-            descriptionLabel.Text = response.EntryDescription;
+            entryInfoView.TimeLabel.Text = secondsToString(response.EntryDuration.DoubleValue);
 
-            timeLabel.Text = secondsToString(response.EntryDuration.DoubleValue);
+            var attributedString = new NSMutableAttributedString("This is an entry with a very long description in three or for or many many lines of text", boldAttributes);
 
             var startTime = DateTimeOffset.FromUnixTimeSeconds(response.EntryStart.LongValue).ToLocalTime();
             var endTime = DateTimeOffset.FromUnixTimeSeconds(response.EntryStart.LongValue + response.EntryDuration.LongValue).ToLocalTime();
             var fromTime = startTime.ToString("HH:mm");
             var toTime = endTime.ToString("HH:mm");
-            timeFrameLabel.Text = $"{fromTime} - {toTime}";
+            var timeFrameString = new NSAttributedString($"\n{fromTime} - {toTime}", regularAttributes);
+
+            attributedString.Append(timeFrameString);
+            entryInfoView.DescriptionLabel.AttributedText = attributedString;
+
+            var width = this.ExtensionContext?.GetHostedViewMaximumAllowedSize().Width ?? 320;
+            var boundingRect = attributedString.GetBoundingRect(new CGSize(width - 135 - 16 * 2, nfloat.MaxValue), NSStringDrawingOptions.UsesLineFragmentOrigin | NSStringDrawingOptions.UsesFontLeading, null);
+
+            View.AddSubview(entryInfoView);
+            var frame = new CGRect(0, 0, width, boundingRect.Height + 12 * 2);
+            entryInfoView.Frame = frame;
+
+            return frame.Size;
         }
 
         [Export("configureWithInteraction:context:completion:")]
